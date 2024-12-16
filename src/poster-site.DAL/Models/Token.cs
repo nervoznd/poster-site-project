c47bc139a140c4658461cc279b4eca865e3cbe7b
kenshi-takahashi <suslikgory12345555@gmail.com>
2024-12-16 13:25:08 +0300

    fix(models): minor syntaxis fix

--- src/poster-site.DAL/Models/Token.cs
+++ src/poster-site.DAL/Models/Token.cs
@@ -8,7 +8,7 @@ namespace poster_site.DAL.Models;
 
 public class Token : BaseEntity
 {
-    public string AccessToken { get; set; }\
+    public string AccessToken { get; set; }
     public string RefreshToken { get; set; }
     public DateTime ExpirationDate { get; set; }
 }
