f024f539365466ea4dc7ade01bb5b2e70ef70028
kenshi-takahashi <suslikgory12345555@gmail.com>
2024-12-16 20:03:25 +0300

    fix(models): change required properties in the BaseMedia entity

--- src/poster-site.DAL/Models/Base/BaseMedia.cs
+++ src/poster-site.DAL/Models/Base/BaseMedia.cs
@@ -11,8 +11,8 @@ public abstract class BaseMedia : BaseEntity
     public string Name { get; set; }
     public string? Description { get; set; }
     public string? ImagePath { get; set; }
-    public DateTime ReleaseDate { get; set; }
-    public bool IsDateAccurate { get; set; }
+    public DateTime? ReleaseDate { get; set; }
+    public bool? IsDateAccurate { get; set; }
     public bool IsReleased { get; set; }
     public string? Genres { get; set; }
 }
