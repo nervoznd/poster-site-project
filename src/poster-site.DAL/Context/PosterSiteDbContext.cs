09981201a923bfe04c38993f8e5d37ac98520039
nervoznd <nervoznd@gmail.com>
2024-12-18 14:17:54 +0300

    feat(dal): add db context

--- src/poster-site.DAL/Context/PosterSiteDbContext.cs
+++ src/poster-site.DAL/Context/PosterSiteDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -11,5 +12,26 @@ namespace poster_site.DAL;
 public class PosterSiteDbContext : DbContext
 {
     public PosterSiteDbContext(DbContextOptions<PosterSiteDbContext> options) : base(options) { }
-    public DbSet<BaseMedia> BaseMedia { get; set; }
+    public DbSet<BaseMedia> BaseMedias { get; set; }
+    public DbSet<Album> Albums { get; set; }
+    public DbSet<Game> Games { get; set; }
+    public DbSet<GameExpansion> GameExpansions { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Series> Series { get; set; }
+    public DbSet<Song> Songs { get; set; }
+    public DbSet<News> News { get; set; }
+    public DbSet<Poster> Posters { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Token> Tokens { get; set; }
+    public DbSet<Topic> Topics { get; set; }
+    public DbSet<TopicSubscriptions> TopicSubscriptions { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
